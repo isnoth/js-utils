6aead00ac83eb6d20387ca533d9cdb33b7da08d3
yaolin <yao.lin@nokia.com>
2018-11-14 12:20:42 +0800

    add writefile

--- src/file.js
+++ src/file.js
@@ -10,6 +10,18 @@ const readFile = (filename, encoding='utf-8')=>{
   })
 }
 
+const writeFile = (path, data, encoding='utf-8')=>{
+  return new Promise((resolve, reject)=>{
+    fs.writeFile(path, data, encoding, (err, data)=>{
+      if(err)
+        reject(err)
+      else
+        resolve(data)
+    })
+  })
+}
+
 module.exports = {
-  readFile
+  readFile,
+  writeFile
 }
