eb4b40ba5ffce59b27c808b17e775d2a21f96193
isnoth <isnothe@gmail.com>
2019-09-09 00:29:02 +0800

    update file

--- src/file.js
+++ src/file.js
@@ -1,22 +1,29 @@
 const fs =require('fs')
+const util = require('util')
+const accessAsync = util.promisify(fs.access)
+
 const readFile = (filename, encoding='utf-8')=>{
   return new Promise((resolve, reject)=>{
-    fs.readFile(filename, encoding, (err, data)=>{
-      if(err)
-        reject(err)
-      else
-        resolve(data)
-    })
+      console.info('readFile:', filename)
+      fs.readFile(filename, encoding, (err, data)=>{
+          if(err) {
+              console.error(err)
+              reject(err)
+          } else{
+              resolve(data)
+          }
+      })
   })
 }
 
 const writeFile = (path, data, encoding='utf-8')=>{
   return new Promise((resolve, reject)=>{
-    fs.writeFile(path, data, encoding, (err, data)=>{
+    fs.writeFile(path, data, encoding, (err, result)=>{
       if(err)
         reject(err)
-      else
-        resolve(data)
+      else{
+          resolve(data)
+      }
     })
   })
 }
@@ -29,17 +36,77 @@ const fileRemove = (name) => {
     })
 }
 
-const fileExists = (name) => {
+const fileExists = function (name) {
+    return accessAsync(name)
+    .then(() => true)
+    .catch(() => false)
+}
+
+const listDir = (path) => {
     return new Promise((res, rej) => {
-        fs.exists(name, (exists)=>{
-            exists? res() : rej()
+        fs.readdir(path, (error, result) => {
+            console.log(path, result)
+            error? rej(error) : res(result)
         })
     })
 }
+
+const rename = (oldPath, newPath, renameFileIfExist=false)  => {
+    return new Promise((res, rej) => {
+        fileExists(newPath)
+        .then((exist) => {
+            console.log('#rename fileExists:', exist)
+            if (!exist){
+                console.log('rename', exist, !exist)
+                fs.rename(oldPath, newPath, error => {
+                    console.log('#rename error:', error)
+                    error? rej(error): res()
+                })
+            } else {
+                if (renameFileIfExist) {
+                    console.log('rename if exist')
+                    fs.rename(oldPath, `${newPath}_${new Date().getTime()}`, error => {
+                        console.log('#rename error:', error)
+                        error? rej(error): res()
+                    })
+
+                } else {
+                    rej(new Error(`#rename dest fileExists:${newPath}`))
+                }
+            }
+        })
+    })
+}
+
+const mkDir = (path) => {
+    return new Promise((res, rej) => {
+        fileExists(path)
+        .then((exist) => {
+            if (exist){
+                const error = new Error(`mkdir ${path} already exist`)
+                rej(error)
+            } else {
+                fs.mkdir(path, { recursive: true }, (error) => {
+                    if (error) {
+                        console.error('mkdir error:', path, error)
+                        rej(error)
+                    } else {
+                        res()
+                    }
+                });
+            }
+        })
+        .catch(err => rej(err))
+    })
+}
+
 
 module.exports = {
   readFile,
   writeFile,
   fileRemove,
-  fileExists
+  fileExists,
+  listDir,
+  rename,
+  mkDir
 }
