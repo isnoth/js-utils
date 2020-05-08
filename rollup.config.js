a9f94dcee8b2839b8b46ec3dc2c0a63d0fea0bb8
yao.lin <yao.lin@nokia-sbell.com>
2020-05-08 02:55:32 +0000

    update cjs amd

--- rollup.config.js
+++ rollup.config.js
@@ -7,5 +7,12 @@ export default [{
     }, {
         file: 'dist/index.iffe.js',
         format: 'iife',
-    }]
+    }, {
+        file: 'dist/index.amd.js',
+        format: 'amd',
+    },{
+        file: 'dist/index.cjs.js',
+        format: 'cjs',
+    }
+    ]
 }];
