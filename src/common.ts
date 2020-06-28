40764580e6f68087852a67da6ef386c608393492
yaolin <yao.lin@nokia-sbell.com>
2020-06-28 10:08:24 +0800

    uid update

--- src/common.ts
+++ src/common.ts
@@ -16,8 +16,9 @@ export function uuidv4() {
     });
 }
 
-export function get10BitUid() {
-    return '_' + Math.random().toString(36).substr(2, 9);
+export function uid() {
+    return (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase()
+    //return '_' + Math.random().toString(36).substr(2, 9);
 }
 
 export class SimpleEventHandler{
