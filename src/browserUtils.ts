91eb2caf6976c4b815e0509dddeade953811bf91
isnoth <isnothe@gmail.com>
2021-11-12 01:52:17 +0000

    add browser base64 encode decode

--- src/browserUtils.ts
+++ src/browserUtils.ts
@@ -107,3 +107,11 @@ export function openNewTab(url: string)  {
         }, 100 );
     }
 }
+
+function utf8_to_b64( str ) {
+    return window.btoa(unescape(encodeURIComponent( str )));
+}
+
+function b64_to_utf8( str ) {
+    return decodeURIComponent(escape(window.atob( str )));
+}
