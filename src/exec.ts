99f3ef3858029ce412af1de444aa8559fcbfb24b
isnoth <isnothe@gmail.com>
2022-03-31 14:55:31 +0800

    exec update

--- src/exec.ts
+++ src/exec.ts
@@ -3,9 +3,10 @@ var exec = require('child-process-promise').exec;
 export const wrappedExec = (cmd)=>{
   return exec(cmd)
   .then(result=>{
-    console.log(result.stdout)
+    return result.stdout
   })
   .catch(err=>{
     console.error(err.stderr)
+    throw err
   })
 }
